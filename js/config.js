842e654376e064178a695398669d6bc73867dea5
Seán <56257946+johnosullivan887@users.noreply.github.com>
2026-07-06 16:20:46 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,47 @@
+// ===============================
+// PVData Configuration
+// ===============================
+
+const CONFIG = {
+
+    xMin: "2015-07-07",
+
+    xMax: "2025-12-12",
+
+    efficiency: {
+
+        min: 15,
+
+        max: 36
+
+    },
+
+    frontTCOColours: {
+
+        "ITO": "#F8A17B",
+
+        "IZO": "#8493AC",
+
+        "Doped-InOx": "#9DB5AA",
+
+        "In-free": "#D29343",
+
+        "Other": "#AA77BB"
+
+    },
+
+    frontTCOMarkers: {
+
+        "ITO": "circle",
+
+        "IZO": "diamond",
+
+        "Doped-InOx": "square",
+
+        "In-free": "triangle-up",
+
+        "Other": "triangle-down"
+
+    }
+
+};
