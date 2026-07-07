fca1c33e2dc2b676704d0cba7bc61eb38e8cc081
Seán <56257946+johnosullivan887@users.noreply.github.com>
2026-07-07 14:45:49 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,44 +4,55 @@
 
 const CONFIG = {
 
-    xMin: "2015-07-07",
+  site: {
+    name: "PVData",
+    version: "0.1"
+  },
 
-    xMax: "2025-12-12",
+  dates: {
+    min: "2015-07-07",
+    max: "2025-12-12"
+  },
 
-    efficiency: {
+  efficiency: {
+    min: 15,
+    max: 36
+  },
 
-        min: 15,
+  indium: {
+    min: 0,
+    max: 11
+  },
 
-        max: 36
+  colours: [
+    "#011959",
+    "#0A285C",
+    "#103F60",
+    "#1C5A62",
+    "#3C6D56",
+    "#687B3E",
+    "#9D892B",
+    "#D29343",
+    "#F8A17B",
+    "#FDB7BC",
+    "#FACCFA"
+  ],
 
-    },
-
-    frontTCOColours: {
-
-        "ITO": "#F8A17B",
-
-        "IZO": "#8493AC",
-
-        "Doped-InOx": "#9DB5AA",
-
-        "In-free": "#D29343",
-
-        "Other": "#AA77BB"
-
-    },
-
-    frontTCOMarkers: {
-
-        "ITO": "circle",
-
-        "IZO": "diamond",
-
-        "Doped-InOx": "square",
-
-        "In-free": "triangle-up",
-
-        "Other": "triangle-down"
-
-    }
+  markerSymbols: [
+    "circle",
+    "diamond",
+    "square",
+    "triangle-up",
+    "triangle-down",
+    "cross",
+    "x",
+    "pentagon",
+    "hexagon",
+    "star",
+    "hourglass",
+    "bowtie",
+    "triangle-left",
+    "triangle-right"
+  ]
 
 };
